64c1150568be4bf87b2c06546b46b0b0e9990bac
mrilyew <99399973+mrilyew@users.noreply.github.com>
2025-09-02 00:10:45 +0300

    make usable

--- venv.ps1
+++ venv.ps1
@@ -2,7 +2,6 @@ $current_location = (Get-Location).Path
 $path = $current_location + "\venv"
 $activate_script = $current_location + "\venv\Scripts\Activate.ps1"
 $src_path = $current_location + "\src"
-$requirements = $current_location + "\requirements.txt"
 
 if (Test-Path -Path $path) {
     echo "venv was found"
@@ -11,6 +10,5 @@ if (Test-Path -Path $path) {
 } else {
     python -m venv $path
     Invoke-Expression $activate_script
-    pip install -r ($requirements)
     Set-Location $src_path
 }
